diamon/minicoredumper

commit 5ebc41520d012bc37238ec9b46807bb08645c32d

(lib)minicoredumper: use regd instead of dbus/inotify
This commit changes minicoredumper to only support the new regd
service. libminicoredumper will automatically register itself
with regd when dump variables are registered.

inotify and dbus support still lingers in libminicoredumper
(and will work with minicoredumper_trigger), but these features
will be completely removed in an upcoming commit.

Signed-off-by: John Ogness <john.ogness@linutronix.de>

src/minicoredumper/corestripper.c
@@ -47,6 +47,7 @@
 #include <sys/mman.h>
 #include <sys/procfs.h>
 #include <sys/syscall.h>
+#include <sys/ptrace.h>
 #include <linux/futex.h>
 #include <elfutils/version.h>
 
@@ -61,6 +62,14 @@
 
 #if _ELFUTILS_PREREQ(0, 167)
 #define SUPPORT_LIBELF_MODIFY
+#endif
+
+#ifndef PTRACE_SEIZE
+#define PTRACE_SEIZE 0x4206
+#endif
+
+#ifndef PTRACE_INTERRUPT
+#define PTRACE_INTERRUPT 0x4207
 #endif
 
 extern int start_dbus_gloop(struct dump_info *di, char *app_name);
@@ -106,79 +115,6 @@ void fatal(const char *fmt, ...)
 	}
 
 	exit(1);
-}
-
-static int trigger_live_dump(struct dump_info *di, char *app_name)
-{
-#ifdef USE_DBUS
-	return start_dbus_gloop(di, app_name);
-#else
-	const char *monitor_fname;
-	char *tmp_path;
-	int err = -1;
-	FILE *f;
-	int fd;
-
-	info("Corestripper live dumper (inotify)");
-	info("dump path    : %s", di->dst_dir);
-	info("dump scope   : %d", di->cfg->prog_config.dump_scope);
-
-	monitor_fname = getenv(DUMP_DATA_MONITOR_ENV);
-	if (!monitor_fname || monitor_fname[0] != '/') {
-		info("%s not defined, not triggering live dump",
-		     DUMP_DATA_MONITOR_ENV);
-		return -1;
-	}
-
-	info("dump trigger : %s", monitor_fname);
-
-	if (asprintf(&tmp_path, "%s/monitor.XXXXXX", di->dst_dir) == -1) {
-		info("error: failed to allocate temp string");
-		return -1;
-	}
-
-	fd = mkstemp(tmp_path);
-	if (fd < 0) {
-		info("error: failed to create temp file");
-		goto out;
-	}
-
-	f = fdopen(fd, "w");
-	if (!f) {
-		info("error: failed to reopen temp file");
-		close(fd);
-		goto out;
-	}
-
-	fprintf(f, "version=%d\n", DUMP_DATA_VERSION);
-	fprintf(f, "scope=%d\n", di->cfg->prog_config.dump_scope);
-	fprintf(f, "path=%s\n", di->dst_dir);
-
-	fclose(f);
-
-	chmod(tmp_path, 0644);
-
-	if (rename(tmp_path, monitor_fname) != 0) {
-		info("error: failed to rename %s to %s", tmp_path,
-		     monitor_fname);
-		unlink(tmp_path);
-		goto out;
-	}
-
-	f = fopen(monitor_fname, "a");
-	if (!f) {
-		info("error: failed to append to %s", monitor_fname);
-		goto out;
-	}
-
-	/* inotify trigger */
-	fclose(f);
-
-	err = 0;
-out:
-	free(tmp_path);
-	return err;
-#endif
 }
 
 static ssize_t read_file_fd(int fd, char *dst, int len)
@@ -488,9 +424,6 @@ static int init_di(struct dump_info *di, int argc, char *argv[])
 			break;
 		comm_base = p + 1;
 	}
-
-	di->dst_dir = alloc_dst_dir(di->timestamp, di->cfg->base_dir,
-				    comm_base, di->pid);
 
 	if (get_task_list(di) != 0)
 		return 1;
@@ -3162,28 +3095,6 @@ static void write_proc_info(struct dump_info *di)
 	copy_proc_files(di, 1, "fd", 1);
 }
 
-static void setup_public_subdir(const char *base, const char *subdir)
-{
-	size_t size;
-	char *name;
-
-	size = strlen(base) + 1 + strlen(subdir) + 1;
-
-	name = malloc(size);
-	if (!name)
-		return;
-
-	snprintf(name, size, "%s/%s", base, subdir);
-
-	mkdir(base, 0755);
-	chmod(base, 0755);
-
-	mkdir(name, 01777);
-	chmod(name, 01777);
-
-	free(name);
-}
-
 #ifdef SUPPORT_LIBELF_MODIFY
 static int add_dumplist_section(struct dump_info *di)
 {
@@ -3284,19 +3195,234 @@ static void do_dump(struct dump_info *di, int argc, char *argv[])
 		/* dump a fat core (if configured) */
 		if (di->cfg->prog_config.dump_fat_core)
 			dump_fat_core(di);
-
-		/* notify registered apps (if configured) */
-		if (di->cfg->prog_config.live_dumper) {
-			setup_public_subdir(di->dst_dir, "proc");
-			setup_public_subdir(di->dst_dir, "dumps");
-			trigger_live_dump(di, argv[0]);
-		}
 	} else {
 		info("dump path: %s", di->dst_dir);
 	}
 out:
 	/* we are done, cleanup */
 	cleanup_di(di);
+}
+
+static long ptrace_tree(enum __ptrace_request request, pid_t pid)
+{
+	char buf[64];
+	struct dirent *de;
+	DIR *d;
+
+	snprintf(buf, sizeof(buf), "/proc/%d/task", pid);
+	d = opendir(buf);
+	if (!d)
+		return -1;
+
+	while (1) {
+		de = readdir(d);
+		if (!de)
+			break;
+		if (de->d_name[0] == '.')
+			continue;
+		ptrace(request, atoi(de->d_name), NULL, NULL);
+	}
+
+	closedir(d);
+
+	return 0;
+}
+
+static int do_lock(pthread_mutex_t *m)
+{
+	int ret;
+
+	ret = pthread_mutex_lock(m);
+	if (ret != 0) {
+		if (ret != EOWNERDEAD)
+			return -1;
+
+		pthread_mutex_consistent(m);
+	}
+
+	return 0;
+}
+
+static void alloc_registered_pids(pid_t core_pid, pid_t **pids, int *n)
+{
+	struct mcd_shm_item *si;
+	struct mcd_shm_head *sh;
+	size_t map_size;
+	struct stat sb;
+	int fd;
+	int i;
+
+	*pids = NULL;
+	*n = 0;
+
+	fd = shm_open(MCD_SHM_PATH, O_RDWR, S_IRUSR|S_IWUSR);
+	if (fd < 0)
+		return;
+
+	if (fstat(fd, &sb) != 0)
+		return;
+
+	map_size = sb.st_size;
+	if (map_size < sizeof(*sh))
+		return;
+
+	sh = mmap(NULL, map_size, PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0);
+	if (sh == MAP_FAILED)
+		return;
+
+	if (do_lock(&sh->m) != 0)
+		goto out;
+
+	if (map_size < sizeof(*sh) + (sh->count * sizeof(*si)))
+		goto out2;
+
+	*pids = malloc(sizeof(pid_t) * sh->count);
+	if (!*pids)
+		goto out2;
+
+	si = (struct mcd_shm_item *)(sh + 1);
+
+	for (i = 0; i < sh->count; i++) {
+		if (si->pid == core_pid) {
+			/* force-unregister core task */
+			si->pid = 0;
+			si->data = 0;
+			sh->count--;
+			info("unregistered core task: %d\n", core_pid);
+		}
+		(*pids)[i] = si->pid;
+		si++;
+	}
+	*n = sh->count;
+out2:
+	pthread_mutex_unlock(&sh->m);
+out:
+	munmap(sh, map_size);
+}
+
+static int do_all_dumps(struct dump_info *di, int argc, char *argv[])
+{
+	const char *recept;
+	bool live_dumper;
+	char *comm_base;
+	pid_t core_pid;
+	char *p;
+	char *ext_argv[10] = {
+		argv[0],
+		argv[1],
+		argv[2],
+		argv[3],
+		"0",
+		argv[5],
+		argv[6],
+		"",
+		argv[8],
+		NULL
+	};
+
+	if (argc == 8) {
+		di->cfg = init_config("/etc/minicoredumper/"
+				      "minicoredumper.cfg.json");
+	} else if (argc == 9) {
+		info("using custom minicoredumper cfg: %s", argv[8]);
+		di->cfg = init_config(argv[8]);
+	} else {
+		fatal("wrong arg count, check /proc/sys/kernel/core_pattern");
+	}
+
+	if (!di->cfg)
+		fatal("unable to init config");
+
+	check_config(di);
+
+	core_pid = strtol(argv[1], &p, 10);
+	if (*p != 0)
+		return 1;
+
+	di->pid = core_pid;
+
+	di->timestamp = strtol(argv[5], &p, 10);
+	if (*p != 0)
+		return 1;
+
+	di->comm = alloc_comm(argv[7], di->pid);
+	if (!di->comm)
+		return 1;
+
+	comm_base = di->comm;
+	while (1) {
+		p = strchr(comm_base, '/');
+		if (!p)
+			break;
+		comm_base = p + 1;
+	}
+
+	di->dst_dir = alloc_dst_dir(di->timestamp, di->cfg->base_dir,
+				    comm_base, di->pid);
+	if (!di->dst_dir)
+		return 1;
+
+	recept = get_prog_recept(di->cfg, di->comm, di->exe);
+	if (!recept)
+		return 1;
+
+	if (init_prog_config(di->cfg, recept) != 0)
+		return 1;
+
+	live_dumper = di->cfg->prog_config.live_dumper;
+
+	cleanup_di(di);
+
+	if (live_dumper) {
+		char pidstr[16];
+		pid_t *pids;
+		int n;
+		int i;
+
+		alloc_registered_pids(core_pid, &pids, &n);
+
+		/* pause all registered tasks */
+		for (i = 0; i < n; i++) {
+			if (pids[i] == 0)
+				continue;
+			if (pids[i] == core_pid)
+				continue;
+			if (ptrace_tree(PTRACE_SEIZE, pids[i]) != 0)
+				pids[i] = 0;
+			else
+				ptrace_tree(PTRACE_INTERRUPT, pids[i]);
+		}
+
+		/* dump all registered tasks */
+		for (i = 0; i < n; i++) {
+			if (pids[i] == 0)
+				continue;
+			if (pids[i] == core_pid)
+				continue;
+			snprintf(pidstr, sizeof(pidstr), "%d", pids[i]);
+			ext_argv[1] = &pidstr[0];
+			do_dump(di, argc, ext_argv);
+		}
+
+		/* resume all registered tasks */
+		for (i = 0; i < n; i++) {
+			if (pids[i] == 0)
+				continue;
+			if (pids[i] == core_pid)
+				continue;
+			ptrace_tree(PTRACE_DETACH, pids[i]);
+		}
+
+		if (pids)
+			free(pids);
+	}
+
+	/* dump crashed task */
+	do_dump(di, argc, argv);
+
+	free(di->dst_dir);
+
+	return 0;
 }
 
 int main(int argc, char *argv[])
@@ -3327,7 +3453,7 @@ int main(int argc, char *argv[])
 		fatal("wrong amount of command line parameters");
 	}
 
-	do_dump(&di, argc, argv);
+	do_all_dumps(&di, argc, argv);
 
 	closelog();
 	munlockall();

src/libminicoredumper/mcd.c
@@ -29,12 +29,15 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <sys/inotify.h>
+#include <sys/socket.h>
+#include <sys/un.h>
 
 #include "dump_data_private.h"
 #include "common.h"
 #include "minicoredumper.h"
 
 static pthread_mutex_t dump_mutex = PTHREAD_MUTEX_INITIALIZER;
+static int registered;
 
 struct mcd_dump_data *mcd_dump_data_head;
 int mcd_dump_data_version = DUMP_DATA_VERSION;
@@ -365,6 +368,101 @@ void mcd_dump_data_dbus_stop(void)
 }
 #endif /* USE_DBUS */
 
+static int mcd_request(int req)
+{
+	uint32_t dval = 0x55555555;
+	struct sockaddr_un addr;
+	struct mcd_regdata data;
+	struct msghdr msgh;
+	struct iovec iov;
+	int err = -1;
+	ssize_t n;
+	int ret;
+	int fd;
+
+	memset(&addr, 0, sizeof(addr));
+	addr.sun_family = AF_UNIX;
+	snprintf(addr.sun_path, sizeof(addr.sun_path), "x%s.%d",
+		 MCD_SOCK_PATH, getpid());
+	addr.sun_path[0] = 0;
+
+	fd = socket(AF_UNIX, SOCK_DGRAM, 0);
+	if (fd < 0)
+		return err;
+
+	ret = bind(fd, (struct sockaddr *)&addr, sizeof(addr));
+	if (ret != 0)
+		goto out;
+
+	memset(&data, 0, sizeof(data));
+	data.req = req;
+	data.data = dval;
+
+	iov.iov_base = &data;
+	iov.iov_len = sizeof(data);
+
+	memset(&addr, 0, sizeof(addr));
+	addr.sun_family = AF_UNIX;
+	snprintf(addr.sun_path, sizeof(addr.sun_path), "x%s", MCD_SOCK_PATH);
+	addr.sun_path[0] = 0;
+
+	memset(&msgh, 0, sizeof(msgh));
+	msgh.msg_iov = &iov;
+	msgh.msg_iovlen = 1;
+	msgh.msg_name = (void *)&addr;
+	msgh.msg_namelen = sizeof(addr);
+
+	do {
+		n = sendmsg(fd, &msgh, 0);
+		if (n < 0 && errno == EINTR)
+			continue;
+		else if (n != sizeof(data))
+			goto out;
+		else
+			break;
+	} while (1);
+
+	do {
+		n = recvmsg(fd, &msgh, 0);
+		if (n < 0 && errno == EINTR)
+			continue;
+		else if (n != sizeof(data))
+			goto out;
+		else
+			break;
+	} while (1);
+
+	if (~data.data != dval)
+		goto out;
+
+	err = 0;
+out:
+	close(fd);
+	return err;
+}
+
+static void handle_register(void)
+{
+	if (registered)
+		return;
+
+	if (mcd_request(MCD_REGISTER) != 0)
+		return;
+
+	registered = 1;
+}
+
+static void handle_unregister(void)
+{
+	if (!registered)
+		return;
+
+	if (mcd_request(MCD_UNREGISTER) != 0)
+		return;
+
+	registered = 0;
+}
+
 static void free_dump_data(struct mcd_dump_data *dd)
 {
 	if (dd->ident)
@@ -413,6 +511,8 @@ static int append_dump_data(struct mcd_dump_data *dump_data)
 	/* add to end of list */
 	iter->next = dump_data;
 	err = 0;
+
+	handle_register();
 out:
 	pthread_mutex_unlock(&dump_mutex);
 
@@ -641,6 +741,8 @@ int mcd_dump_data_unregister(mcd_dump_data_t dd)
 		free_dump_data(iter);
 	else
 		err = ENOKEY;
+
+	handle_unregister();
 
 	pthread_mutex_unlock(&dump_mutex);
 

src/minicoredumper/Makefile.am
@@ -25,7 +25,8 @@ minicoredumper_SOURCES += dbus_mcd.c
 dbus_CFLAGS += -DUSE_DBUS -I$(top_srcdir)/src/dbus -I$(top_srcdir)/src/compat
 endif
 
-minicoredumper_LDADD = $(top_builddir)/lib/libjson.a -lthread_db -lrt \
+minicoredumper_LDADD = $(top_builddir)/lib/libjson.a \
+		       -lthread_db -lrt -lpthread \
 		       $(dbus_LIBS) \
 		       $(libelf_LIBS)
 minicoredumper_CFLAGS = -g -Wall -std=c99 -D_GNU_SOURCE $(MCD_WERROR) \

commit 2477ea83e5fa193bbf553c33f2f969b62a8e6116

minicoredumper: fix broken core dumping
Core dumping was rendered completely broken by commit
5ebc41520d012bc37238ec9b46807bb08645c32d because cleanup_di()
was called too early, thus causing the kernel to cleanup the
process before minicoredumper was finished. The use of
cleanup_di() was just a lazy way of freeing temporary data
used to acquire the dump destination from the configuration.
Rather than misuse this function, a separate set of temporary
variables have been defined that are then manually freed.

Signed-off-by: John Ogness <john.ogness@linutronix.de>

src/minicoredumper/corestripper.c
@@ -160,9 +160,9 @@ static ssize_t write_file_fd(int fd, char *src, int len)
 	return size;
 }
 
-static void check_config(struct dump_info *di)
+static void check_config(struct config *cfg)
 {
-	if (!di->cfg->base_dir)
+	if (!cfg->base_dir)
 		fatal("no base_dir set in config file");
 }
 
@@ -262,6 +262,9 @@ static char *alloc_comm(char *arg, pid_t pid)
 	if (arg[0] != 0)
 		return strdup(arg);
 
+	if (pid == 0)
+		return NULL;
+
 	if (asprintf(&tmp_path, "/proc/%i/comm", pid) == -1)
 		return NULL;
 
@@ -286,6 +289,38 @@ static char *alloc_comm(char *arg, pid_t pid)
 	fclose(f);
 
 	return p;
+}
+
+static char *alloc_exe(pid_t pid)
+{
+	char *tmp_path;
+	char *exe;
+	int ret;
+
+	if (pid == 0)
+		return NULL;
+
+	exe = malloc(PATH_MAX + 1);
+	if (!exe)
+		return NULL;
+
+	if (asprintf(&tmp_path, "/proc/%i/exe", pid) == -1) {
+		free(exe);
+		return NULL;
+	}
+
+	ret = readlink(tmp_path, exe, PATH_MAX + 1);
+	if (ret < 0 || ret > PATH_MAX) {
+		info("readlink on \'%s\' failed", tmp_path);
+		free(tmp_path);
+		free(exe);
+		return NULL;
+	}
+	free(tmp_path);
+	/* readlink does not terminate the string */
+	exe[ret] = 0;
+
+	return exe;
 }
 
 static char *alloc_dst_dir(time_t timestamp, const char *base_dir,
@@ -332,7 +367,6 @@ static int init_di(struct dump_info *di, int argc, char *argv[])
 	char *comm_base;
 	char *tmp_path;
 	char *p;
-	int ret;
 
 	if (elf_version(EV_CURRENT) == EV_NONE) {
 		info("elf_version EV_NONE");
@@ -372,22 +406,9 @@ static int init_di(struct dump_info *di, int argc, char *argv[])
 	if (!di->comm)
 		return 1;
 
-	di->exe = malloc(PATH_MAX + 1);
+	di->exe = alloc_exe(di->pid);
 	if (!di->exe)
 		return 1;
-
-	if (asprintf(&tmp_path, "/proc/%i/exe", di->pid) == -1)
-		return 1;
-
-	ret = readlink(tmp_path, di->exe, PATH_MAX + 1);
-	if (ret < 0 || ret > PATH_MAX) {
-		info("readlink on \'%s\' failed", tmp_path);
-		free(tmp_path);
-		return 1;
-	}
-	free(tmp_path);
-	/* readlink does not terminate the string */
-	di->exe[ret] = 0;
 
 	if (argc == 8) {
 		di->cfg = init_config("/etc/minicoredumper/"
@@ -402,7 +423,7 @@ static int init_di(struct dump_info *di, int argc, char *argv[])
 	if (!di->cfg)
 		fatal("unable to init config");
 
-	check_config(di);
+	check_config(di->cfg);
 
 	info("comm: %s", di->comm);
 	info("exe: %s", di->exe);
@@ -3302,10 +3323,14 @@ out:
 
 static int do_all_dumps(struct dump_info *di, int argc, char *argv[])
 {
+	struct config *cfg;
 	const char *recept;
 	bool live_dumper;
 	char *comm_base;
 	pid_t core_pid;
+	long timestamp;
+	char *comm;
+	char *exe;
 	char *p;
 	char *ext_argv[10] = {
 		argv[0],
@@ -3321,35 +3346,40 @@ static int do_all_dumps(struct dump_info *di, int argc, char *argv[])
 	};
 
 	if (argc == 8) {
-		di->cfg = init_config("/etc/minicoredumper/"
-				      "minicoredumper.cfg.json");
+		cfg = init_config("/etc/minicoredumper/"
+				  "minicoredumper.cfg.json");
 	} else if (argc == 9) {
 		info("using custom minicoredumper cfg: %s", argv[8]);
-		di->cfg = init_config(argv[8]);
+		cfg = init_config(argv[8]);
 	} else {
 		fatal("wrong arg count, check /proc/sys/kernel/core_pattern");
 	}
 
-	if (!di->cfg)
+	if (!cfg)
 		fatal("unable to init config");
 
-	check_config(di);
+	check_config(cfg);
 
 	core_pid = strtol(argv[1], &p, 10);
 	if (*p != 0)
 		return 1;
 
-	di->pid = core_pid;
-
-	di->timestamp = strtol(argv[5], &p, 10);
+	timestamp = strtol(argv[5], &p, 10);
 	if (*p != 0)
 		return 1;
 
-	di->comm = alloc_comm(argv[7], di->pid);
-	if (!di->comm)
+	comm = alloc_comm(argv[7], core_pid);
+	if (!comm)
 		return 1;
 
-	comm_base = di->comm;
+	if (core_pid == 0)
+		exe = strdup("");
+	else
+		exe = alloc_exe(core_pid);
+	if (!exe)
+		return 1;
+
+	comm_base = comm;
 	while (1) {
 		p = strchr(comm_base, '/');
 		if (!p)
@@ -3357,21 +3387,23 @@ static int do_all_dumps(struct dump_info *di, int argc, char *argv[])
 		comm_base = p + 1;
 	}
 
-	di->dst_dir = alloc_dst_dir(di->timestamp, di->cfg->base_dir,
-				    comm_base, di->pid);
+	di->dst_dir = alloc_dst_dir(timestamp, cfg->base_dir,
+				    comm_base, core_pid);
 	if (!di->dst_dir)
 		return 1;
 
-	recept = get_prog_recept(di->cfg, di->comm, di->exe);
+	recept = get_prog_recept(cfg, comm, exe);
 	if (!recept)
 		return 1;
 
-	if (init_prog_config(di->cfg, recept) != 0)
+	if (init_prog_config(cfg, recept) != 0)
 		return 1;
 
-	live_dumper = di->cfg->prog_config.live_dumper;
+	live_dumper = cfg->prog_config.live_dumper;
 
-	cleanup_di(di);
+	free_config(cfg);
+	free(comm);
+	free(exe);
 
 	if (live_dumper) {
 		char pidstr[16];