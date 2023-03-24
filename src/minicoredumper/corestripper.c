diamon/minicoredumper

commit a09461321cb8bdb941c9d4e45f5ab79f9dd5b4f1

minicoredumper: treat eof as error
The kernel should never return 0 when reading from the core stream
because the minicoredumper should only be reading in valid vma
blocks. But add a check in case something bizarre happens and abort
the read.

Signed-off-by: John Ogness <john.ogness@linutronix.de>

src/minicoredumper/corestripper.c
@@ -976,6 +976,11 @@ static int copy_data(int src, int dest, int dest2, size_t len, char *pagebuf)
 			/* skip this chunk */
 			lseek64(src, chunk, SEEK_CUR);
 			memset(pagebuf, 0, chunk);
+
+		} else if (ret == 0) {
+			info("read core eof-failed at 0x%lx",
+			     lseek64(src, 0, SEEK_CUR));
+			return -1;
 		}
 
 		ret = write_file_fd(dest, pagebuf, chunk);

commit 5bc4beb4c605470f4566d1f7563b9a6b5c45b3af

minicoredumper: add eof check for read_file_fd()
Commit a09461321cb8 ("minicoredumper: treat eof as error") added an eof
check to the higher-level copy_data() function, assuming that the
lower-level read_file_fd() could return 0. But actually that function
was not handling the eof and would instead go into an infinite loop.

read() can return 0 if the crashing process is killed (for example with
explicit SIGKILL) before the minicoredumper is finished reading the
memory.

The problem was reported and fixed by Dongyang. The original report:

    In the below situation, the read will in an infinite loop:

    If the PID 123 triggered the core dump and Minicoredumper started
    to read the /proc/123/mem (dump_compressed_tar-->copy_data-->read_file_fd).
    At this time, if the OOM is triggered, then the PID 123 is killed by
    SIGKILL. The /proc/123/ folder will nonexistent, the read will return zero. 

    But in the read_file_fd, the code doesn't check the zero condition.
    When the "r" is 0,  the "len" will never to 0,  then into an infinite loop. 

Fixes: a09461321cb8 ("minicoredumper: treat eof as error")
Signed-off-by: Dongyang <dongyang626@gmail.com>
Signed-off-by: Jianyu <jianyu9785@gmail.com>
Signed-off-by: John Ogness <john.ogness@linutronix.de>

src/minicoredumper/corestripper.c
@@ -125,6 +125,9 @@ static ssize_t read_file_fd(int fd, char *dst, int len)
 		if (r > 0) {
 			size += r;
 			len -= r;
+		} else {
+			info("Couldn't read file fd=%d; unexpected eof", fd);
+			return r;
 		}
 	} while (len > 0);
 